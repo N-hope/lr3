85c9e15dc1f2e48d8567d16c04e2b5e6ac68f549
N-hope <nadiaK3009@gmail.com>
2022-11-28 01:04:49 +0200

    #2

--- script.js
+++ script.js
@@ -1,35 +1,28 @@
-//var masivDatPriym[5];
+function T(){
+  var now = new Date();
+  var pib = "";
+  var table = document.body.children[3];
+    for(var i=1; i<6; i++){
+		var people=table.rows[i].cells[0].innerHTML;
+		pib= people;
+		
+		var DateHuman = new Date(table.rows[i].cells[1].innerHTML);
+		var y = parseInt(((now-DateHuman)/1000/3600/24/365));
+		var D = new Date(table.rows[i].cells[2].innerHTML += y + " років");
+		console.log(pib +" "+ y);
+	}
+}
+
 /*
-//воно↓ працює
+//↓працює
 document.getElementById("dtTbl").onclick=function(){
 var table = document.body.children[3];
 for(var i=1; i<6; i++){
 	a= alert(table.rows[i].cells[1].innerHTML);
 	}
 }
-
 var now = new Date();
 var d1 = new Date(2007, 8, 27);
 	var y = ((now-d1)/3600/24/1000);
 	alert( y +"days or " +(y/365)+ " years");
 */
-function onClck(){
-var table = document.body.children[3];
-console.dir(table);
-
-	/*
-var table = document.body.table;
-var a1 = table.rows[1].cells[1].innerHTML;
-alert( a1 ); */
-/*document.getElementById("dtTbl").onclick=function(){
-var table = document.body.children[3];
-for(var j=0; j<5; j++){
-for(var i=1; i<6; i++){
-masivDatPriym[j]=table.rows[i].cells[1].innerHTML;
-}}
-}
-/*document.getElementById("dt").onclick=function(){
-for(var i=0; i<5; i++){
-alert(masivDatPriym[i]);
-}
-}*/
