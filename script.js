e76e1494ed8d739227d2b092224611328440baef
N-hope <nadiaK3009@gmail.com>
2022-11-28 00:34:42 +0200

    #1

---
+++ script.js
@@ -0,0 +1,35 @@
+//var masivDatPriym[5];
+/*
+//воно↓ працює
+document.getElementById("dtTbl").onclick=function(){
+var table = document.body.children[3];
+for(var i=1; i<6; i++){
+	a= alert(table.rows[i].cells[1].innerHTML);
+	}
+}
+
+var now = new Date();
+var d1 = new Date(2007, 8, 27);
+	var y = ((now-d1)/3600/24/1000);
+	alert( y +"days or " +(y/365)+ " years");
+*/
+function onClck(){
+var table = document.body.children[3];
+console.dir(table);
+
+	/*
+var table = document.body.table;
+var a1 = table.rows[1].cells[1].innerHTML;
+alert( a1 ); */
+/*document.getElementById("dtTbl").onclick=function(){
+var table = document.body.children[3];
+for(var j=0; j<5; j++){
+for(var i=1; i<6; i++){
+masivDatPriym[j]=table.rows[i].cells[1].innerHTML;
+}}
+}
+/*document.getElementById("dt").onclick=function(){
+for(var i=0; i<5; i++){
+alert(masivDatPriym[i]);
+}
+}*/
