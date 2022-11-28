5c3d578036f8043202c1f1da6422e48eb8f2fa77
N-hope <nadiaK3009@gmail.com>
2022-11-28 02:18:48 +0200

    #4 - JS

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function T(){
   var now = new Date();
   var pib = "";
-  var table = document.body.children[3];
+  var table = document.body.children[2];
     for(var i=1; i<6; i++){
-		var people=table.rows[i].cells[0].innerHTML;
-		pib= people;
+		var people = table.rows[i].cells[0].innerHTML;
+		pib = people;
 		
 		var DateHuman = new Date(table.rows[i].cells[1].innerHTML);
 		var y = parseInt(((now-DateHuman)/1000/3600/24/365));
@@ -12,6 +12,7 @@ function T(){
 		console.log(pib +" "+ y);
 	}
 }
+//<script src="script.js" defer></script>
 
 /*
 //↓працює
